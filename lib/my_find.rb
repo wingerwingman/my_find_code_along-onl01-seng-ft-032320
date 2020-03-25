eef17b72e37aa133c56f30d41e95276ba2e07aee
Justin Winger <justinwinger4@gmail.com>
2020-03-25 19:42:59 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,8 +4,7 @@ require 'pry'
 def my_find(collection)
   i = 0 
   while i < collection.length 
-    if yield(collection[i])
-      return collection[i]
+    return collection[i] if yield(collection[i])
     i += 1 
   end
 
